1d5f9a64162b59d8dc4e4fea336b3bc8a250b2c4
Rony Vargas <soyronyvargas@gmail.com>
2023-12-29 18:09:48 -0500

    feat: barra volumen

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -2,12 +2,14 @@ import { create } from 'zustand'
 
 export const usePlayerStore = create((set) => ({
   isPlaying: false,
+  volume: 1,
   currentMusic: {
     playlist: null,
     song: null,
     songs: []
   },
   setIsPlaying: (value) => set((state) => ({ isPlaying: value })),
+  setVolume: (value) => set(() => ({ volume: value })),
   setCurrentMusic: (currentMusic) => set(() => ({ currentMusic }))
 //   removeAllBears: () => set({ bears: 0 }),
 }))
